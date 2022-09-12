49f34e52e8588969ba5a46267cb158fa9f7e7357
Aurelian-C <aurelian_reghin@yahoo.com>
2022-09-12 23:48:14 +0300

    Update

--- src/js/Views/HeaderView.js
+++ src/js/Views/HeaderView.js
@@ -2,18 +2,22 @@ class HeaderView {
   _header = document.querySelector('.header');
 
   constructor() {
-    window.addEventListener('wheel', e => {
-      console.log(e.wheelDelta);
-      if (e.wheelDelta < 0) {
-        this._header.classList.add('header--scroll-down');
-        this._header.classList.remove('header--scroll-up');
-      }
-
-      if (e.wheelDelta > 0) {
-        this._header.classList.remove('header--scroll-down');
-        this._header.classList.add('header--scroll-up');
-      }
+    // Add a 'wheel' event for the mobile phones
+    ['wheel'].forEach(event => {
+      window.addEventListener(event, this._toggleHeaderVisibility.bind(this));
     });
+  }
+
+  _toggleHeaderVisibility(e) {
+    if (e.wheelDelta < 0) {
+      this._header.classList.add('header--scroll-down');
+      this._header.classList.remove('header--scroll-up');
+    }
+
+    if (e.wheelDelta > 0) {
+      this._header.classList.remove('header--scroll-down');
+      this._header.classList.add('header--scroll-up');
+    }
   }
 }
 
