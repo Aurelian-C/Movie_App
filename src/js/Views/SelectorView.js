3cb3a71ea9d39d94dd22d950254fa03bc87b9456
Aurelian-C <aurelian_reghin@yahoo.com>
2022-09-13 16:20:54 +0300

    Update

--- src/js/Views/SelectorView.js
+++ src/js/Views/SelectorView.js
@@ -4,17 +4,27 @@ class SelectorView extends View {
   addHandlerSelect(handler) {
     const element = this._root.querySelector('.selector__container');
     element.addEventListener('click', e => {
-      // Select clicked item
-      const item = e.target.closest('.selector__item');
+      // Make container visible
+      const itemsContainer = e.target.closest('.selector__items');
+      itemsContainer.classList.add('active');
+
+      // Select clicked item that NOT have .selected class
+      const item = e.target.closest('.selector__item:not(.selected)');
       if (!item) return;
 
       // Remove 'selected' class
       const parentEl = item.closest('.selector__container');
       const childrensEl = parentEl.querySelectorAll('.selector__item');
-      childrensEl.forEach(childEl => childEl.classList.remove('selected'));
+
+      childrensEl.forEach(childEl => {
+        childEl.classList.remove('selected');
+      });
 
       // Add 'selected' class to clicked item
       item.classList.add('selected');
+
+      // Close the container by hidding
+      itemsContainer.classList.remove('active');
 
       // Get container
       const sectionEl = parentEl.closest('.cards-section');
@@ -28,15 +38,19 @@ class SelectorView extends View {
   _generateMarkup(data) {
     return `
       <div class="selector">
-        <h3 class="selector__description">${data.title}</h3>
-        <ul class="selector__container">
-          ${data.items
-            .map((item, i) => {
-              const data = { item, firstItem: i ? '' : true };
-              return this._generateMarkupItems(data);
-            })
-            .join('')}
-        </ul>
+        <div class="selector__container">
+          <h3 class="selector__description">${data.title}</h3>
+            <div class="selector__wrapper">
+              <ul class="selector__items">
+                ${data.items
+                  .map((item, i) => {
+                    const data = { item, firstItem: i ? '' : true };
+                    return this._generateMarkupItems(data);
+                  })
+                  .join('')}
+              </ul>
+          </div>
+        </div>
       </div>
     `;
   }
@@ -47,8 +61,8 @@ class SelectorView extends View {
 
     return `
       <li class="selector__item ${selected}" data-media-type="${mediaType}">
-        <h4 class="selector__title">${item}</h4>
         <div class="selector__bg"></div>
+        <h4 class="selector__title">${item}</h4>
       </li>
     `;
   }
