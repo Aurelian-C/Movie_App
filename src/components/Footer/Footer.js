58304ac59ad3cffc61e37a7827b6f2a7d362aa58
Aurelian-C <aurelian_reghin@yahoo.com>
2023-05-28 11:07:03 +0300

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,23 +4,14 @@ import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
-    <footer className={`${classes.footer} u-margin-top-big`}>
-      <div className={classes.footer__container}></div>
+    <footer className={`${classes.footer}`}>
       <div className={classes.footer__logo}>
         <img src={logo} alt="" className={classes.footer__img} />
       </div>
       <div className={classes.footer__copyright}>
         <p>
-          This website is inspired by
-          <Link
-            className={classes['footer__copyright-link']}
-            target="_blank"
-            to="https://www.themoviedb.org/"
-          >
-            TMDB
-          </Link>
-          , build from scratch with HTML, CSS, Vanilla JavaScript, ReactJS &amp;
-          React Router.
+          This website is build from scratch with HTML, CSS, Vanilla JavaScript,
+          ReactJS &amp; React Router.
         </p>
         <p>
           For more projects go to
