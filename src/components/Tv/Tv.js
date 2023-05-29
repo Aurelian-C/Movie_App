7db3bda7be72042d5ddf28be6a21d34eb966e2e7
Aurelian-C <aurelian_reghin@yahoo.com>
2023-05-29 21:10:18 +0300

    update

--- src/components/Tv/Tv.js
+++ src/components/Tv/Tv.js
@@ -1,5 +1,83 @@
 import classes from './Tv.module.css';
+import { useContext } from 'react';
+import { ModeDarkContext } from '../../store/dark-mode';
 
-export default function Tv() {
-  return <div>TV</div>;
+export default function Tv({ tvDetail }) {
+  const darkModeContext = useContext(ModeDarkContext);
+  const light =
+    'linear-gradient(to right, rgb(82, 105, 162), rgba(67, 22, 19, 0.6))';
+  const dark =
+    'linear-gradient(to right, rgba(41, 41, 41, 1), rgba(0, 0, 0,  0.6))';
+
+  let liniarGradient = darkModeContext.darkMode ? dark : light;
+
+  return (
+    <div
+      className={classes.header}
+      style={{
+        backgroundImage: `${liniarGradient}, url(${tvDetail.backdrop_path})`,
+      }}
+    >
+      <div className={classes['header__container']}>
+        <div className={classes['header__poster']}>
+          <img src={tvDetail.poster_path} alt={tvDetail.title} />
+        </div>
+        <div className={classes['header__details']}>
+          <div className={classes['header__title']}>
+            <h2>{tvDetail.title}</h2>
+            <div className={classes['header__facts']}>
+              <div>{tvDetail.release_date}</div>
+              <div className={classes['header__genres']}>
+                {tvDetail.genres.map((genre, i) => {
+                  return i !== tvDetail.genres.length - 1 ? (
+                    <span key={i}>{genre.name},</span>
+                  ) : (
+                    <span key={i}>{genre.name}</span>
+                  );
+                })}
+              </div>
+              <div>
+                {tvDetail.runtime.hours}h {tvDetail.runtime.minutes}m
+              </div>
+            </div>
+          </div>
+          <div className={classes['header__actions']}>
+            <div className={classes['header__user-score']}>
+              <div className={classes['header__score']}>
+                <span>{tvDetail.vote_average}</span>
+              </div>
+              <div className={classes['header__user']}>User score</div>
+            </div>
+            <div className={classes['header__buttons']}>
+              <button className={classes['header__button']}>
+                <i className="fa-regular fa-rectangle-list"></i>
+                <div className={classes['header__tooltip']}>
+                  Login to create and edit custom list
+                </div>
+              </button>
+              <button className={classes['header__button']}>
+                <i className="fa-solid fa-heart"></i>
+                <div className={classes['header__tooltip']}>
+                  Login to add this movie to your favorite list
+                </div>
+              </button>
+              <button className={classes['header__button']}>
+                <i className="fa-solid fa-bookmark"></i>
+                <div className={classes['header__tooltip']}>
+                  Login to add this movie to your watchlist
+                </div>
+              </button>
+            </div>
+          </div>
+          <div className={classes['header__info']}>
+            <div className={classes['header__tagline']}>{tvDetail.tagline}</div>
+            <div className={classes['header__overview']}>
+              <h3>Overview</h3>
+              <p>{tvDetail.overview}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/TvDetails.js
+++ src/pages/TvDetails.js
@@ -1,16 +1,38 @@
 import Tv from '../components/Tv/Tv';
+import PersonCard from '../components/Cards/PersonCard/PersonCard';
 import { API_KEY, API_URL } from '../config/config';
-import { defer } from 'react-router-dom';
-import { useEffect } from 'react';
+import { createCastDetails, createMovieDetails } from '../config/helpers';
+import { Await, defer, useLoaderData } from 'react-router-dom';
+import { Suspense, useEffect } from 'react';
 
 export default function TvDetails() {
+  const { tv, credits } = useLoaderData();
+  console.log(tv);
+  console.log(credits);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
     <>
-      <Tv />
+      <Suspense fallback={<p>Loading...</p>}>
+        <Await resolve={tv}>
+          {movie => {
+            const tvDetail = createMovieDetails(tv);
+            return <Tv tvDetail={tvDetail} />;
+          }}
+        </Await>
+      </Suspense>
+
+      <Suspense fallback={<p>Loading...</p>}>
+        <Await resolve={credits}>
+          {credits => {
+            const castDetails = createCastDetails(credits.cast);
+            return <PersonCard castDetail={castDetails} />;
+          }}
+        </Await>
+      </Suspense>
     </>
   );
 }
@@ -35,7 +57,7 @@ export async function fetchTvCredits(params) {
 // Loader for TVs
 export async function tvLoader({ params }) {
   return defer({
-    movie: await fetchTvDetails(params),
+    tv: await fetchTvDetails(params),
     credits: fetchTvCredits(params),
   });
 }
