ca97285787106d0a430b2b81704cf7cac4c5ee64
Aurelian-C <aurelian_reghin@yahoo.com>
2023-06-08 13:05:39 +0300

    update

--- src/components/CastAndCrew/CastAndCrew.js
+++ src/components/CastAndCrew/CastAndCrew.js
@@ -1,7 +1,13 @@
+import SectionPageSecondary from '../UI/SectionWrapper/SectionPageSecondary';
 import classes from './CastAndCrew.module.css';
 
 export default function CastAndCrew({ cast, crew }) {
   console.log('cast:', cast);
   console.log('crew:', crew);
-  return <h1 className={classes.test}>Cast & Crew Details</h1>;
+
+  return (
+    <SectionPageSecondary className={classes.test}>
+      <h1>Cast & Crew Details</h1>
+    </SectionPageSecondary>
+  );
 }
