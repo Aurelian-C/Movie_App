fde46b6564f4693a7938da59dc8da454c735ec35
Aurelian-C <aurelian_reghin@yahoo.com>
2023-07-21 19:35:05 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
-import './index.css';
+import './main.css';
 import App from './App';
 import ModeDarkContextProvider from './store/dark-mode';
 
