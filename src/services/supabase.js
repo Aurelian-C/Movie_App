43c6e634f85d13cef5da1e839be4cba6f9e42db6
Aurelian-C <aurelian_reghin@yahoo.com>
2024-01-24 21:31:10 +0200

    update

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://oxznxvtqeqncqcusahld.supabase.co';
+const supabaseUrl = 'https://kktqerjtjyxhysrtocbc.supabase.co';
 const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94em54dnRxZXFuY3FjdXNhaGxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY5MTczNjUsImV4cCI6MjAxMjQ5MzM2NX0.KNKnar4q7VKMS9RW6aXXKaarIqqlFET7COtPgg7FARk';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrdHFlcmp0anl4aHlzcnRvY2JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYxMjM0NTUsImV4cCI6MjAyMTY5OTQ1NX0.9KouSB5NsuxFd-Sy32RxPlaqMdO686td7CwxbuIaHOI';
 export const supabase = createClient(supabaseUrl, supabaseKey);
